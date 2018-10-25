3951f71875e18f25d11e8b7c2fb8dc3b38bb1dff
Christopher Wang <czw@mit.edu>
2018-10-24 22:46:52 -0400

    diffuse light

--- Material.cpp
+++ Material.cpp
@@ -1,9 +1,12 @@
 #include "Material.h"
+using namespace std;
 Vector3f Material::shade(const Ray &ray,
     const Hit &hit,
     const Vector3f &dirToLight,
     const Vector3f &lightIntensity)
 {
     // TODO implement Diffuse and Specular phong terms
-    return Vector3f(0, 0, 0);
+    float dot = Vector3f::dot(hit.normal, dirToLight);
+    float clamp = dot>0 ? dot:0;
+    return clamp*lightIntensity*_diffuseColor;
 }
